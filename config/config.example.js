24e41a21f3652d05663b4e8aacc088c68bcf0ba4
Tarun Gupta <tarun.gupta@venturepact.com>
2013-12-31 12:05:02 +0530

    Added config.example.js

---
+++ config/config.example.js
@@ -0,0 +1,92 @@
+
+var path = require('path')
+  , rootPath = path.normalize(__dirname + '/..')
+  , templatePath = path.normalize(__dirname + '/../app/mailer/templates');
+
+module.exports = {
+  development: {
+    db: 'mongodb://localhost:27017/about_me',
+    root: rootPath,
+    //notifier: notifier,
+    app: {
+      name: 'Nodejs Passport Boilerplate Demo'
+    },
+    facebook: {
+      clientID: "332362696893520",
+      clientSecret: "c469c6e2282aaff72171279d1cecc8ad",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://APP_URL/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://APP_URL/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://APP_URL/auth/google/callback"
+    },
+  },
+  test: {
+    db: 'mongodb://localhost:27017/about_me',
+    root: rootPath,
+    //notifier: notifier,
+    app: {
+      name: 'Nodejs Express Mongoose Demo'
+    },
+    facebook: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://localhost:3000/auth/google/callback"
+    }
+  },
+  production: {
+  db: 'mongodb://localhost:27017/about_me',
+    root: rootPath,
+    //notifier: notifier,
+    app: {
+      name: 'Nodejs Passport Boilerplate Demo'
+    },
+    facebook: {
+      clientID: "332362696893520",
+      clientSecret: "c469c6e2282aaff72171279d1cecc8ad",
+      callbackURL: "http://node.venturepact.com/auth/facebook/callback"
+    },
+    twitter: {
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
+      callbackURL: "http://APP_URL/auth/twitter/callback"
+    },
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://APP_URL/auth/github/callback'
+    },
+    google: {
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
+      callbackURL: "http://APP_URL/auth/google/callback"
+    },
+  
+  }
+}
